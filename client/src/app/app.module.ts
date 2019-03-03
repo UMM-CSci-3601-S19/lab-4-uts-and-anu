5a01ea1b1752151ce15809a38ebdabb9db1e92cb
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-03 14:33:23 -0600

    Change Filtering By Status to Checkbox Add Checkbox for todos where status are true Add MatCheckbox Module to app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {HomeComponent} from './home/home.component';
 import {UserComponent} from './users/user.component';
 import {UserListComponent} from './users/user-list.component';
 import {UserListService} from './users/user-list.service';
+import {TodoComponent} from "./todos/todo.component";
 import {TodoListComponent} from "./todos/todo-list.component";
 import {TodoListService} from "./todos/todo-list.service";
 import {Routing} from './app.routes';
@@ -17,6 +18,7 @@ import {APP_BASE_HREF} from '@angular/common';
 
 import {CustomModule} from './custom.module';
 import {AddUserComponent} from './users/add-user.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -25,6 +27,7 @@ import {AddUserComponent} from './users/add-user.component';
     HttpClientModule,
     Routing,
     CustomModule,
+    MatCheckboxModule,
   ],
   declarations: [
     AppComponent,
@@ -33,6 +36,7 @@ import {AddUserComponent} from './users/add-user.component';
     UserComponent,
     AddUserComponent,
     TodoListComponent,
+    TodoComponent,
   ],
   providers: [
     UserListService,
