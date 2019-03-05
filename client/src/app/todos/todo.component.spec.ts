d7220f3cbe50d8f242ecad4fd2d772a4f8bb900a
teklu009 <teklu009@morris.umn.edu>
2019-03-04 20:52:14 -0600

    finialize test for todo component

--- client/src/app/todos/todo.component.spec.ts
+++ client/src/app/todos/todo.component.spec.ts
@@ -1,43 +1,75 @@
 import {ComponentFixture, TestBed, async} from '@angular/core/testing';
-import {User} from './user';
-import {UserComponent} from './user.component';
-import {UserListService} from './user-list.service';
+import {Todo} from './todo';
+import {TodoComponent} from './todo.component';
+import {TodoListService} from './todo-list.service';
 import {Observable} from 'rxjs/Observable';
 import {CustomModule} from "../custom.module";
 
-describe('User component', () => {
+describe('Todo component', () => {
 
-  let userComponent: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
+  let todoComponent: TodoComponent;
+  let fixture: ComponentFixture<TodoComponent>;
 
-  let userListServiceStub: {
-    getUserById: (userId: string) => Observable<User>
+  let todoListServiceStub: {
+    getTodoById: (todoId: string) => Observable<Todo>
   };
 
   beforeEach(() => {
     // stub UserService for test purposes
-    userListServiceStub = {
-      getUserById: (userId: string) => Observable.of([
+    todoListServiceStub = {
+      getTodoById: (todoId: string) => Observable.of([
         {
-          _id: 'chris_id',
-          name: 'Chris',
-          age: 25,
-          company: 'UMM',
-          email: 'chris@this.that'
+          _id: 'ananya_id',
+          owner: 'Ananya',
+          status: true,
+          body: 'abcdefg',
+          category: 'UMM'
+
         },
         {
           _id: 'pat_id',
-          name: 'Pat',
-          age: 37,
-          company: 'IBM',
-          email: 'pat@something.com'
+          owner: 'Pat',
+          status: true,
+          body: 'dddddd',
+          category: 'IBM'
+
         },
         {
           _id: 'jamie_id',
-          name: 'Jamie',
-          age: 37,
-          company: 'Frogs, Inc.',
-          email: 'jamie@frogs.com'
+          owner: 'Jamie',
+          status: false,
+          body: 'ssssss',
+          category: 'Frogs'
+
         }
-      ].find(user => user._id === userId))
+      ].find(todo => todo._id === todoId))
     };
+
+    TestBed.configureTestingModule({
+      imports: [CustomModule],
+      declarations: [TodoComponent],
+      providers: [{provide: TodoListService, useValue: todoListServiceStub}]
+    });
+  });
+
+
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoComponent);
+      todoComponent = fixture.componentInstance;
+    });
+  }));
+
+  it('can retrieve Ananya by ID', () => {
+    todoComponent.setId('ananya_id');
+    expect(todoComponent.todo).toBeDefined();
+    expect(todoComponent.todo.owner).toBe('Ananya');
+    expect(todoComponent.todo.body).toBe('abcdefg');
+  });
+
+  it('returns undefined for Santa', () => {
+    todoComponent.setId('Santa');
+    expect(todoComponent.todo).not.toBeDefined();
+  });
+
+});
