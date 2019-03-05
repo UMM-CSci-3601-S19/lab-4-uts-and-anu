fbde4ef93751a48563efe89256a1008a1117951a
teklu009 <teklu009@morris.umn.edu>
2019-03-04 20:08:25 -0600

    todo component spec in progress

---
+++ client/src/app/todos/todo.component.spec.ts
@@ -0,0 +1,23 @@
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {TestBed} from '@angular/core/testing';
+import {HttpClient} from '@angular/common/http';
+
+import {Todo} from './todo';
+import {TodoListService} from './todo-list.service';
+
+describe('Todo list service: ', () => {
+
+  const testTodos: Todo[] = [
+    {
+      
+    },
+    {
+
+    },
+    {
+
+    }
+  ];
+
+
+});
